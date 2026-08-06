b0d4d947f7f45d0b7d2c7af2db84fc63613f5858
diljott3-byte <diljott3@gmail.com>
2026-08-06 22:16:57 +0530

    Update shared contact and map configuration

--- site-config.js
+++ site-config.js
@@ -6,11 +6,11 @@ window.SITE_CONFIG = Object.freeze({
   brandName: "SANKALP",
   parentBrand: "Jesan Steel Works",
   establishedYear: "2018",
-  location: "Chintpurni Road, Hoshiarpur, Punjab",
+  location: "Chohal, Hoshiarpur, Punjab",
   showroomAddress:
-    "Chintpurni Road, Near Hotel Royal Plaza, New Colony Chohal, Hoshiarpur, Punjab 146024",
+    "V.P.P. New Colony, Chohal, Near Hotel Royal Plaza, Hoshiarpur, Punjab",
   showroomMapUrl:
-    "https://www.google.com/maps/search/?api=1&query=Jesan%20Steel%20Works%2C%20Chintpurni%20Road%2C%20Near%20Hotel%20Royal%20Plaza%2C%20New%20Colony%20Chohal%2C%20Hoshiarpur%2C%20Punjab%20146024",
+    "https://www.google.com/maps/search/?api=1&query=Jesan%20Steel%20Works%2C%20V.P.P.%20New%20Colony%2C%20Chohal%2C%20Near%20Hotel%20Royal%20Plaza%2C%20Hoshiarpur%2C%20Punjab",
   whatsappNumber: "919041095780",
   phoneNumber: "+919041095780",
   phoneDisplay: "+91 90410 95780",
